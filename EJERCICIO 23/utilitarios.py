90bc4a1e256ddf22a8c3c9d12abc07f15957ca31
MBacon019 <marcelobacon18@gmail.com>
2024-12-03 19:21:11 -0500

    Add files via upload

---
+++ EJERCICIO 23/utilitarios.py
@@ -0,0 +1,9 @@
+def calcularPromedio(val1,val2,val3):
+    calculo=val1+val2+val3
+    promedio1=calculo/3
+    return promedio1
+
+def consultarMulta(tipo1,tipo2,tipo3,tipo4,tipo5,tipo6):
+    calculo= tipo1 + tipo2 
+    porcentaje= calculo / 100
+    return porcentaje
