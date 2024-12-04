fb9531d206423de35f13620e6b268deaf94f5236
SA-1078 <santiagocolimba10@gmail.com>
2024-12-03 20:01:00 -0500

    corregidoSubtotal

--- EJERCICIO 23/utilitarios.py
+++ EJERCICIO 23/utilitarios.py
@@ -7,3 +7,15 @@ def consultarMulta(tipo1,tipo2,tipo3,tipo4,tipo5,tipo6):
     calculo= tipo1 + tipo2 
     porcentaje= calculo / 100
     return porcentaje
+
+def calcularSubtotal(precioProducto: float, cantidad: int, porcentajeDescuento: float) -> float:
+    # Calcular el subtotal sin descuento
+    subtotalSinDescuento = precioProducto * cantidad
+
+    # Calcular el valor del descuento
+    descuento = (subtotalSinDescuento * porcentajeDescuento) / 100
+
+    # Calcular el subtotal con descuento aplicado
+    subtotal = subtotalSinDescuento - descuento
+
+    return subtotal
